9f6048e7b7065ad401615b64831657d93ac3d95b
Maxin-C <57470650+Maxin-C@users.noreply.github.com>
2020-02-25 12:24:20 +0800

    change vocabulary count to word count

--- unit2/homework2_count.py
+++ unit2/homework2_count.py
@@ -1,7 +1,14 @@
 import os.path
+
 if os.path.isfile('./readme.md'):
     with open('./readme.md','rb') as sth:
-        count=0
-        for line in sth:
-            count += len(line)
-        print(count)
+        list=sth.readlines()
+words=0
+for lines in list:
+    lines=bytes.decode(lines)
+    lines=lines.replace(',','')
+    lines=lines.strip()
+    wo=lines.split(' ')
+    words += len(wo)
+
+print(words)
